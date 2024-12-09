6a8d86cb4038614906eb6bc14bfc6cdca1e2fc74
ensea <ensea@StudentLab>
2024-12-09 11:44:11 +0100

    TP1

--- TP_1/quest1.c
+++ TP_1/quest1.c
@@ -3,9 +3,18 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdio.h>
-
+#define ENSEASH "enseash % "
 int main() {
 	const char *mess ="$ ./enseash Bienvenue dans le Shell ENSEA. Pour quitter, tapez 'exit'.enseash %\n";
+	
+    char commande[256];
+    
+    write(STDOUT_FILENO, mess, strlen(mess));
+	while(1){
+		write(STDOUT_FILENO, mess, strlen(ENSEASH));
+        ssize_t n = read(STDIN_FILENO, commande, 256);
+    
+	}
 	write(STDOUT_FILENO, mess, sizeof(mess));
     exit(EXIT_SUCCESS);
 }

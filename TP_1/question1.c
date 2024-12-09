3d41f9ba31f9c235ec0074ea7f36ddb7c834571a
ensea <ensea@StudentLab>
2024-12-09 09:58:08 +0100

    TP1 Q1

--- TP_1/question1.c
+++
@@ -1,9 +0,0 @@
-#include <sys/types.h>
-#include <unistd.h>
-#include <stdlib.h>
-
-int main() {
-	const char mess="$ ./enseash Bienvenue dans le Shell ENSEA. Pour quitter, tapez 'exit'.enseash %";
-	write(STDOUT_FILEN0, mess, strlen(mess));
-	EXIT_SUCCESS;
-}
